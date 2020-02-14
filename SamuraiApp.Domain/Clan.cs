1ee90efd0333b60583a68c23c5d61583ec0ae43b
Simmy Rai <Simmy.Rai@edsembli.com>
2020-02-14 13:46:28 -0500

    Installed EntityFrameworkCoreV3

---
+++ SamuraiApp.Domain/Clan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+    }
+}
