2218b0296341ab641e354c902eeaa76c4026dcc4
Simmy Rai <Simmy.Rai@edsembli.com>
2020-02-14 14:03:16 -0500

    Established DbContext file

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+    }
+}
